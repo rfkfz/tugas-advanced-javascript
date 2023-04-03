6bd86e7caa69b8e81d1a16b55082f8131fc523c4
rifkifauzi-versia <rifkifauzi.versia@gmail.com>
2023-04-03 16:10:05 +0700

    number 1 finish

---
+++ belajar.js
@@ -0,0 +1,19 @@
+//NUMBER 1
+let person = {
+    author: "John Red Doe",
+    title: "Mr.RED has a red and a green car",
+    images: {
+        url: "https://dummy.com/red-image.jpg",
+    }
+};
+
+for (let key of Object.keys(person)) {
+    if (typeof person[key] == 'string') {
+        person[key] = person[key].replace('red', 'blue');
+        person[key] = person[key].replace('RED', 'blue');
+        person[key] = person[key].replace('Red', 'blue');
+    }
+}
+person.images.url = person.images.url.replace('red', 'blue');
+
+console.log(person);
